08c02d85e374257836e639298a134cfe1b16eb88
N.D.Thuan <thuan.nguyennunu@hcmut.edu.vn>
2020-08-04 09:43:42 +0700

    update

--- abc.py
+++ abc.py
@@ -1,69 +1,94 @@
 import pandas as pd
 import sys
 import numpy as np
+import stopwords
 
-file = r'a.csv'
-df = pd.read_csv(file)
 
-df.query('(posTag == "Ny") or (posTag == "Np") or (nerLabel == "B-ORG") or (nerLabel == "B-LOC") or (nerLabel == "B-PER") or (nerLabel == "I-ORG") or (nerLabel == "I-LOC") or (nerLabel == "I-PER") or ((posTag == "N" or posTag == "A" or posTag == "V") and nerLabel == "O")', inplace = True)
 
-df = df.replace(r'[^aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ\-_]', '', regex=True)
+def preprocessing(file = "result99.csv"):
+    """
+    input: filename 
 
-df["form"] = df["form"].str.lower()
+    output: dataframe after preprocessing
+    """
+    df = pd.read_csv(file)
+    # Filter to get Noun, Verb, Object, Location/Organization, Person.
+    df.query('(posTag == "Ny") or (posTag == "Np") or (nerLabel == "B-ORG") or (nerLabel == "B-LOC") or (nerLabel == "B-PER") or (nerLabel == "I-ORG") or (nerLabel == "I-LOC") or (nerLabel == "I-PER") or ((posTag == "N" or posTag == "A" or posTag == "V") and nerLabel == "O")', inplace = True)
+    # Filter unicode
+    df = df.replace(r'[^aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ\-_]', '', regex=True)
+    # Lower case
+    df["form"] = df["form"].str.lower()
 
-df_postProcessing = df
-# print(df_postProcessing)
+    return df
+
+# df_postProcessing = df
 # df_postProcessing.to_csv('data_postprocessing.csv', index=False, encoding='utf-8-sig')
 
-df_person = df\
-# df.drop_duplicates()
 
-# print(a)
+def merge2Rows(df, prev, i, curRow):
+    prev[1].form = str(prev[1].form) + "_" + str(curRow.form)
+    df.at[prev[0], "form"] = prev[1].form
+def get_df_person(file = "result99.csv"):
+    df = preprocessing(file)
+    # Query person
+    df.query('(nerLabel == "B-PER") or (nerLabel == "I-PER")', inplace = True)
+    # Merge rows to get full-name person
+    for i, curRow in df.iterrows():
+        if (curRow.nerLabel == "I-PER"):
+            merge2Rows(df, prev, i, curRow) 
+        else:
+            prev = [i, curRow]
+    # Query to get full-name person
+    df.query('(nerLabel == "B-PER")', inplace = True)
+    # return df
 
-df_person.query('(nerLabel == "B-PER") or (nerLabel == "I-PER")', inplace = True)
+    dup = df.groupby(['form']).size().reset_index(name="counts")
+    dup.sort_values(by=['counts'], ascending=False, inplace=True)
+    print(dup)
 
-# df_temp.sort_values(by=['depLabel'], ascending=False, inplace=True)
-
-# print(df_temp)
-
-# def foo(x):
-#     print(x[0])
-
-# df_person.apply(lambda x: foo(x) , axis = 1)
-# print(df_person)
-# df_person.to_csv('z.csv', index=False, encoding='utf-8-sig')
+get_df_person()
 
 
-def merge2Rows(prevRow, j, i):
-    prevRow[1].form = str(prevRow[1].form) + "_" + str(j.form)
-    df_person.at[prevRow[0], "form"] = prevRow[1].form
+# df_person = df
+
+# df_keywords = df_person.groupby(['form']).size().reset_index(name="counts")
+# dup.sort_values(by=['counts'], ascending=False, inplace=True)
+# print(dup)
 
 
-for i, j in df_person.iterrows():
-    if (j.nerLabel == "I-PER"):
-        merge2Rows(prevRow, j, i) 
-    else:
-        prevRow = [i, j]
 
-df_person.query('(nerLabel == "B-PER")', inplace = True)
-# print(df_person)
-# df_person.to_csv('z.csv', index=False, encoding='utf-8-sig')
 
-# df_temp = df_person.groupby(['form']).agg(np.size)
-# df_temp.sort_values(by=['depLabel'], ascending=False, inplace=True)
+# # df_temp.sort_values(by=['depLabel'], ascending=False, inplace=True)
+
+# # print(df_temp)
+
+# # def foo(x):
+# #     print(x[0])
+
+# # df_person.apply(lambda x: foo(x) , axis = 1)
+# # print(df_person)
 # # df_person.to_csv('z.csv', index=False, encoding='utf-8-sig')
-# print(df_temp["form"])
-
-
-# dup = df_person.pivot_table(index="form",aggfunc={'form': np.size})
-dup = df_person.groupby(['form']).size().reset_index(name="counts")
-dup.sort_values(by=['counts'], ascending=False, inplace=True)
-print(dup)
-dup.to_csv('z.csv', index=False, encoding='utf-8-sig')
 
 
 
 
+
+# dup = df_person.groupby(['form']).size().reset_index(name="counts")
+# dup.sort_values(by=['counts'], ascending=False, inplace=True)
+# print(dup)
+# dup.to_csv('z.csv', index=False, encoding='utf-8-sig')
+
+
+sw = stopwords.getStopwordsVN_ENG()
+full = preprocessing()
+arr = ["là", "có", "khi", "người", "được", "nhiều", "bạn", "tôi", "ngày", "phải", "nó", "đi" + "_"]
+full['form'] = full['form'].apply(lambda x: ''.join([word for word in x.split() if word not in (sw + arr)]))
+full['form'].replace('', np.nan, inplace=True)
+
+
+full = full.dropna()
+print(full)
+full.to_csv('z.csv', index=False, encoding='utf-8-sig')
 
 
 
